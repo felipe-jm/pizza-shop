7d9e806f00d03395bb2a3d299b2bdbe09eca192a
Felipe Jung <felipemattoseu@gmail.com>
2024-08-03 13:33:19 -0400

    Add revenue chart daterange picker

---
+++ src/api/get-daily-revenue-in-period.ts
@@ -0,0 +1,28 @@
+import { api } from "@/lib/axios";
+
+export interface GetDailyRevenueInPeriodQuery {
+  from?: Date;
+  to?: Date;
+}
+
+export interface GetDailyRevenueInPeriodResponse {
+  date: number;
+  receipt: number;
+}
+
+export async function getDailyRevenueInPeriod({
+  from,
+  to,
+}: GetDailyRevenueInPeriodQuery) {
+  const response = await api.get<GetDailyRevenueInPeriodResponse[]>(
+    "/metrics/daily-receipt-in-period",
+    {
+      params: {
+        from: from?.toISOString(),
+        to: to?.toISOString(),
+      },
+    },
+  );
+
+  return response.data;
+}
