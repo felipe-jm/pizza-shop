9202a698e3638a6c5c208b867ffe0b981217ccbf
Felipe Jung <felipemattoseu@gmail.com>
2024-08-10 09:26:13 -0400

    Add await to expect that expects elements to be visible

--- test/sign-up.e2e-spec.ts
+++ test/sign-up.e2e-spec.ts
@@ -12,7 +12,7 @@ test("sign up successfully", async ({ page }) => {
 
   const toast = await page.getByText("Restaurante cadastrado com sucesso!");
 
-  expect(toast).toBeVisible();
+  await expect(toast).toBeVisible();
 });
 
 test("sign up with error", async ({ page }) => {
@@ -27,7 +27,7 @@ test("sign up with error", async ({ page }) => {
 
   const toast = await page.getByText("Erro ao cadastrar o restaurante.");
 
-  expect(toast).toBeVisible();
+  await expect(toast).toBeVisible();
 });
 
 test("navigate to new login page", async ({ page }) => {
